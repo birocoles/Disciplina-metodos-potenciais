acf4166bddcbc920bb663f3dcdd110514cc3859d
Vanderlei C Oliveira Jr <vandscoelho@gmail.com>
2015-10-27 12:10:31 -0200

    atualizacao das minhas funcoes e do notebook sobre o eigen6c4

--- Aulas/Modelos_globais_grav/minhas_funcoes.py
+++ Aulas/Modelos_globais_grav/minhas_funcoes.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def WGS84():
     '''
@@ -119,3 +120,154 @@ def gamma_closedform(a, f, GM, omega, phi, h):
     gamma = (10**5)*gamma/W
     
     return gamma
+    
+def estatistica(dado, unidade=None):
+    '''
+    Calcula os valores minimo, medio, maximo e 
+    diferenca entre o maximo e o minimo de um
+    dado.
+    
+    input
+    
+    dado: numpy array 1D - vetor de dados.
+    unidade: string - unidade dos dados.
+    
+    output
+    
+    minimo: float - minimo dos dados.
+    media: float - media dos dados.
+    maximo: float - maximo dos dados.
+    variacao: float - diferenca entre o maximo e o
+              minimo dos dados.
+    '''
+    
+    assert dado.size > 1, 'O vetor de dados deve ter mais de um elemento'
+    
+    minimo = np.min(dado)
+    maximo = np.max(dado)
+    media = np.mean(dado)
+    variacao = maximo - minimo
+    
+    if (unidade != None):
+        print '     min.: %15.5f %s' % (minimo, unidade)
+        print '    media: %15.5f %s' % (media, unidade)
+        print '     max.: %15.5f %s' % (maximo, unidade)
+        print 'var. max.: %15.5f %s' % (variacao, unidade)
+    else:
+        print '     min.: %15.5f' % (minimo)
+        print '    media: %15.5f' % (media)
+        print '     max.: %15.5f' % (maximo)
+        print 'var. max.: %15.5f' % (variacao)
+        
+    return minimo, media, maximo, variacao
+    
+def plota_mapa(projecao, x, y, dado, area, unidade, titulo, cores, tamanho,
+               delta, perfis=None, estados=None, escala=None, eixos=None):
+    '''
+    Plota um mapa dos dados "dado", com coordenadas "x" e 
+    "y" referidas a uma determinada projecao cartografica 
+    "projecao". "unidade" e "titulo" sao, respectivamente,
+    a unidade dos dados e o titulo do mapa.
+    
+    input
+    
+    projecao: objeto do mpl_toolkits.basemap.Basemap - 
+              projecao cartografica.
+    x: numpy array 1D - vetor com as coordenadas x
+       da projecao.
+    y: numpy array 1D - vetor com as coordenadas y
+       da projecao.
+    dado: numpy array 1D - vetor com os dados a serem
+          plotados.
+    area: list - lista com os valores minimo e maximo da
+          longitude e minimo e maximo da latitude, em
+          graus.
+    unidade: string - unidade dos dados a serem plotados.
+    titulo: string -  titulo do mapa.
+    cores: codigo do colormaps_reference.py - esquema de 
+           cores do mapa.
+    tamanho: tuple - define os tamanhos tx e ty do mapa ao longo,
+             repectivamente, dos eixos x e y. Os valores tx e ty
+             sao passados entre parenteses e separados por virgula
+             (tx, ty).
+    delta: float - intervalo, em graus, entre os meridiando e paralelos.
+    perfis: numpy array 2D - matriz com as coordenadas x e y
+            dos pontos iniciais e finais dos perfis a serem 
+            analisados. As coordenadas x e y estao na primeira
+            e segunda colunas, respectivamente. As primeiras
+            duas linhas contem os dois pontos que formam o 
+            primeiro perfil, as proximas duas contem os pontos
+            que formam o segundo perfil e assim sucessivamente.
+    estados: boolean - se for igual a True, plota o contorno dos estados.
+    escala: boolean - se for igual a True, plota a escala do mapa.
+    eixos: boolean - se for igual a True, plota os eixos do mapa.
+    
+    output
+    
+    mapa: string - codigo de uma matplotlib.figure.Figure.
+    '''
+
+    dado_min = np.min(dado)
+    dado_max = np.max(dado)
+    
+    #Esquema da escala de cores
+    if (dado_min*dado_max < 0.):
+        ranges = np.max(np.abs([dado_min, dado_max]))
+        ranges_0 = 0.
+    else:
+        ranges = 0.5*(dado_max - dado_min)
+        ranges_0 = 0.5*(dado_max + dado_min)
+    
+    longitude_central = 0.5*(area[1] + area[0])
+    latitude_central = 0.5*(area[3] + area[2])
+    
+    x_max = np.max(x)*0.001 # valor maximo de x em km
+    x_min = np.min(x)*0.001 # valor minimo de x em km
+    
+    if escala == True:
+    
+        #Valor em km a ser representado na escala
+        #Este valor foi estabelecido como aproximadamente 
+        #40 porcento da variacao maxima em x
+        comprimento_escala = np.floor(0.4*(x_max - x_min)/100.)*100.
+    
+        #Posicao do centro da escala em coordenadas geodesicas
+        longitude_escala = area[1] - 0.25*(area[1] - area[0])
+        latitude_escala = area[2] + 0.05*(area[3] - area[2])
+    
+    x_min = np.min(x)
+    x_max = np.max(x)
+    y_min = np.min(y)
+    y_max = np.max(y)
+    
+    plt.figure(figsize=tamanho)
+    plt.title(titulo, fontsize=18, y=1.05)
+    projecao.contourf(x, y, dado, 100, tri=True, cmap=plt.get_cmap(cores),
+                      vmin = -ranges + ranges_0, vmax = ranges + ranges_0)
+    plt.colorbar(orientation='horizontal', pad=0.04, aspect=50, 
+                 shrink=0.7).set_label(unidade, fontsize=18)
+    projecao.drawcoastlines()
+    if (np.ceil(area[2]) == area[2]):
+        parallels = np.arange(np.ceil(area[2]) + 1., area[3], delta)
+    else:
+        parallels = np.arange(np.ceil(area[2]), area[3], delta)
+    if (np.ceil(area[0]) == area[0]):
+        meridians = np.arange(np.ceil(area[0]) + 1., area[1], delta)
+    else:
+        meridians = np.arange(np.ceil(area[0]), area[1], delta)
+    if eixos == True:
+        projecao.drawparallels(parallels, labels=[1,1,0,0])
+        projecao.drawmeridians(meridians, labels=[0,0,1,1])
+    else:
+        projecao.drawparallels(parallels)
+        projecao.drawmeridians(meridians)
+    if estados == True:
+        projecao.drawstates()
+    if perfis != None:
+        for i in range(0,perfis.shape[0],2):
+            projecao.plot(perfis[i:i+2,0], perfis[i:i+2,1], 'o-k', linewidth=2)
+    if escala == True:
+        projecao.drawmapscale(longitude_escala, latitude_escala,
+                            longitude_central, latitude_central,
+                            length=comprimento_escala, barstyle='fancy')    
+    plt.show()
